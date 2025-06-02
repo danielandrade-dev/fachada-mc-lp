6054ebad7f9b6a3256dce84738b43d4b682f1962
Daniel Andrade <danielthooth@gmail.com>
2025-06-01 22:46:16 -0300

    Adiciona novas imagens à configuração do portfólio, expandindo a galeria com mais opções visuais.

--- app/config/portfolio.ts
+++ app/config/portfolio.ts
@@ -20,6 +20,54 @@ export const portfolioImages: PortfolioImage[] = [
     }, {
         src: '/galeria/5.jpg',
         alt: 'Descrição da Imagem 5'
+    }, {
+        src: '/galeria/6.jpg',
+        alt: 'Descrição da Imagem 6'
+    }, {
+        src: '/galeria/7.jpg',
+        alt: 'Descrição da Imagem 7'
+    }, {
+        src: '/galeria/8.jpg',
+        alt: 'Descrição da Imagem 8'
+    }, {
+        src: '/galeria/9.jpg',
+        alt: 'Descrição da Imagem 9'
+    }, {
+        src: '/galeria/10.jpg',
+        alt: 'Descrição da Imagem 10'
+    }, {
+        src: '/galeria/11.jpg',
+        alt: 'Descrição da Imagem 11'
+    }, {
+        src: '/galeria/12.jpg',
+        alt: 'Descrição da Imagem 12'
+    }, {
+        src: '/galeria/13.jpg',
+        alt: 'Descrição da Imagem 13'
+    }, {
+        src: '/galeria/14.jpg',
+        alt: 'Descrição da Imagem 14'
+    }, {
+        src: '/galeria/15.jpg',
+        alt: 'Descrição da Imagem 15'
+    }, {
+        src: '/galeria/16.jpg',
+        alt: 'Descrição da Imagem 16'
+    }, {
+        src: '/galeria/17.jpg',
+        alt: 'Descrição da Imagem 17'
+    }, {
+        src: '/galeria/18.jpg',
+        alt: 'Descrição da Imagem 18'
+    }, {
+        src: '/galeria/19.jpg',
+        alt: 'Descrição da Imagem 19'
+    }, {
+        src: '/galeria/20.jpg',
+        alt: 'Descrição da Imagem 20'
+    }, {
+        src: '/galeria/21.jpg',
+        alt: 'Descrição da Imagem 21'
     },
     // Adicione mais imagens aqui
 ]; 
